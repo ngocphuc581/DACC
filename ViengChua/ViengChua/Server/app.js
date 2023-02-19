53135c98835aa4607a3f92dde381927f17a41544
Lê Nguyễn Trung Hiếu <lenguyentrunghieu2001@gmail.com>
2023-02-19 17:11:12 +0700

    trunghieu 19/2/2023

--- ViengChua/ViengChua/Server/app.js
+++ ViengChua/ViengChua/Server/app.js
@@ -6,10 +6,20 @@ const MongooseURI =
 
 mongoose.set('strictQuery', true);
 
+require('./User');
+require('./PhapDanh');
+require('./CaDao');
+require('./Phat');
+const User = mongoose.model('User');
+const PhapDanh = mongoose.model('PhapDanh');
+const CaDao = mongoose.model('CaDao');
+const Phat = mongoose.model('Phat');
+
 mongoose.connect(MongooseURI, {
   useNewUrlParser: true,
 });
 mongoose.set('strictQuery', true);
+
 mongoose.connection.on('Connected', () => {
   console.log('Connect DB Success');
 });
