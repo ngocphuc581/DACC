6c833639b219cedbcdca8e4cad073b25eb43f4a3
Lê Nguyễn Trung Hiếu <lenguyentrunghieu2001@gmail.com>
2023-02-21 21:22:59 +0700

    edit app.js 2

--- ViengChua/ViengChua/Server/app.js
+++ ViengChua/ViengChua/Server/app.js
@@ -49,6 +49,11 @@ app.post('/ViengChua/createUser', async (req, res) => {
   res.send(user);
 });
 
+app.post('/ViengChua/checkAccount', async (req, res) => {
+  const check = await User.findOne({username: req.body.username});
+  res.send(check);
+});
+
 app.post('/ViengChua/changePassword', async (req, res) => {
   const changePassword = await User.findByIdAndUpdate(
     {
